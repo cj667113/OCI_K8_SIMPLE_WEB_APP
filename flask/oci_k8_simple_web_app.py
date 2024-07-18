f30aa6a3749673b0bc6f958390f32a1f454edca3
Oracle Public Cloud User <opc@johnston-docker-image-builder.johnstonvcn1sub.johnstonvcn1.oraclevcn.com>
2024-07-18 16:43:32 +0000

    Flask App

---
+++ flask/oci_k8_simple_web_app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+import requests
+app = Flask(__name__)
+
+@app.route('/')
+def fetch_data():
+	url = 'http://169.254.169.254/opc/v2/instance/hostname'
+	headers = {'Authorization': 'Bearer Oracle'}
+	response=requests.get(url,headers=headers)
+	if response.status_code==200:
+		return response.text
+	else:
+		return f'Failed to fetch data: {response.status_code}'
+if __name__ == '__main__':
+	app.run(host='0.0.0.0',port=80,debug=True)
